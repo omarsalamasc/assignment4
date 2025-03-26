c93f82fdb934e1563915dd192ec7ef435c41a2ee
Omar Salama <salamaom@sheridancollege.ca>
2025-03-26 18:09:00 -0400

    Version2 PhoneRec.py - Saved and read JSON with formatted phonebook output

--- phoneRec.py
+++ phoneRec.py
@@ -1,9 +1,15 @@
+import json
+
 phonebook = {
     "John": ("209 Trafalgar Road", "905-666-7777"),
     "Rosie": ("1439 Trafalgar Road", "487-423-7721"),
     "Alex": ("5 Main Street", "905-222-1234")
 }
 
-with open("SpeedDial1.txt", "w") as file:
-    for info in phonebook.values():
-        file.write(info[1] + "\n")
+with open("Speeddial2.json", "w") as file:
+    json.dump(phonebook, file, indent=4)
+
+with open("Speeddial2.json", "r") as file:
+    data = json.load(file)
+    for name, value in data.items():
+        print(name + ": " + value[0] + " - " + value[1])
